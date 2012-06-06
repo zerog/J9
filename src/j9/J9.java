cdc9adaf0986ae3ed97beda30ca628304fb93ee2
Christopher Lemire <christopher.lemire@gmail.com>
2012-06-06 12:17:00 -0500

    For fucks sake

--- src/j9/J9.java
+++ src/j9/J9.java
@@ -5,8 +5,11 @@
 
 package j9;
 
-import java.awt.GridBagLayout;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,12 +20,18 @@ import javax.swing.SwingUtilities;
  * @author Christopher Lemire <christopher.lemire@gmail.com>
  * @author Travis Mosley <tprowx@gmail.com>
  */
-public class J9 extends JPanel {
+public class J9 extends JPanel implements ActionListener {
   private static JFrame frame;
   private JButton b1, b2, b3, b4, b5, b6, b7, b8, b9;
-	GridBagConstraints c = new GridBagConstraints();
+  private Dimension dim;
+	private GridBagConstraints constraints;
 
   public J9() {
+    // Instantiate
+    constraints = new GridBagConstraints();
+    dim = new Dimension(66, 40);
+
+    // Set options
     setLayout(new GridBagLayout()); //FIXME Write the constructor
 
     // Create buttons
@@ -54,28 +63,50 @@ public class J9 extends JPanel {
             + "<center><font color=red>9</font></center>"
             + "<p><font color=green>wxyz</font>");
 
+    // Set size (deminsions)
+    b1.setSize(dim);
+    b2.setSize(dim);
+    b3.setSize(dim);
+    b4.setSize(dim);
+    b5.setSize(dim);
+    b6.setSize(dim);
+    b7.setSize(dim);
+    b8.setSize(dim);
+    b9.setSize(dim);
+
+    // Add event listeners
+    b1.addActionListener(this);
+    b2.addActionListener(this);
+    b3.addActionListener(this);
+    b4.addActionListener(this);
+    b5.addActionListener(this);
+    b6.addActionListener(this);
+    b7.addActionListener(this);
+    b8.addActionListener(this);
+    b9.addActionListener(this);
+
     // Add components
-		c.gridx = 0;
-		c.gridy = 0;
-    add(b1, c);
-		c.gridx = 1;
-    add(b2, c);
-		c.gridx = 2;
-    add(b3, c);
-		c.gridx = 0;
-		c.gridy = 1;
-    add(b4, c);
-		c.gridx = 1;
-    add(b5, c);
-		c.gridx = 2;
-    add(b6, c);
-		c.gridx = 0;
-		c.gridy = 3;
-    add(b7, c);
-		c.gridx = 1;
-    add(b8, c);
-		c.gridy = 4;
-    add(b9, c);
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+    add(b1, constraints);
+		constraints.gridx = 1;
+    add(b2, constraints);
+		constraints.gridx = 2;
+    add(b3, constraints);
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+    add(b4, constraints);
+		constraints.gridx = 1;
+    add(b5, constraints);
+		constraints.gridx = 2;
+    add(b6, constraints);
+		constraints.gridx = 0;
+		constraints.gridy = 3;
+    add(b7, constraints);
+		constraints.gridx = 1;
+    add(b8, constraints);
+		constraints.gridy = 4;
+    add(b9, constraints);
   }
 
   private static void createAndShowGUI() {
@@ -98,5 +129,10 @@ public class J9 extends JPanel {
     SwingUtilities.invokeLater(new Runnable() {
       public void run() { createAndShowGUI(); }
     });
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    System.out.println(((JButton)e.getSource()).getSize());
   }
 }

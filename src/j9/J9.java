b26534356fa801d80959763b81890a35b92f9824
Christopher Lemire <christopher.lemire@gmail.com>
2012-06-06 10:34:56 -0500

    HTML button labels

--- src/j9/J9.java
+++ src/j9/J9.java
@@ -18,22 +18,38 @@ import javax.swing.SwingUtilities;
  */
 public class J9 extends JPanel {
   private static JFrame frame;
-  private JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b0;
+  private JButton b1, b2, b3, b4, b5, b6, b7, b8, b9;
 
   public J9() {
     setLayout(new GridBagLayout()); //FIXME Write the constructor
 
     // Create buttons
-    b1 = new JButton("1");
-    b2 = new JButton("2\nabc");
-    b3 = new JButton("3");
-    b4 = new JButton("4");
-    b5 = new JButton("5");
-    b6 = new JButton("6");
-    b7 = new JButton("7");
-    b8 = new JButton("8");
-    b9 = new JButton("9");
-    b0 = new JButton("0");
+    b1 = new JButton("<html>"
+            + "<center><font color=red>1</font></center>");
+    b2 = new JButton("<html>"
+            + "<center><font color=red>2</font></center>"
+            + "<p><font color=green>abc</font>");
+    b3 = new JButton("<html>"
+            + "<center><font color=red>3</font></center>"
+            + "<p><font color=green>def</font>");
+    b4 = new JButton("<html>"
+            + "<center><font color=red>4</font></center>"
+            + "<p><font color=green>ghi</font>");
+    b5 = new JButton("<html>"
+            + "<center><font color=red>5</font></center>"
+            + "<p><font color=green>jkl</font>");
+    b6 = new JButton("<html>"
+            + "<center><font color=red>6</font></center>"
+            + "<p><font color=green>mno</font>");
+    b7 = new JButton("<html>"
+            + "<center><font color=red>7</font></center>"
+            + "<p><font color=green>qrs</font>");
+    b8 = new JButton("<html>"
+            + "<center><font color=red>8</font></center>"
+            + "<p><font color=green>tuv</font>");
+    b9 = new JButton("<html>"
+            + "<center><font color=red>9</font></center>"
+            + "<p><font color=green>wxyz</font>");
 
     // Add components
     add(b1);
@@ -45,7 +61,6 @@ public class J9 extends JPanel {
     add(b7);
     add(b8);
     add(b9);
-    add(b0);
   }
 
   private static void createAndShowGUI() {
